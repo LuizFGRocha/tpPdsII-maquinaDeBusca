5a8e5ffa7204e17b2d2ec0b12e07bf7fabe1a644
Luiz Fernando <luizfgrocha04@gmail.com>
2022-11-10 13:13:58 -0300

    Busca usa normalizar(). constroiIndice a debugar.

--- src/indice_invertido.cpp
+++ src/indice_invertido.cpp
@@ -1,17 +1,29 @@
 #include "indice_invertido.h"
-#include <vector>
 #include <filesystem>
 #include <string>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
 set<string> Indice_Invertido::busca(string palavra) {
-    return indice_[palavra];
+    return indice_[normalizar(palavra)];
 }
 
 void Indice_Invertido::inserir(string palavra, string documento) {
     indice_[palavra].insert(documento);
+}
+
+void Indice_Inverso::constroiIndice(){
+    const filesystem::path documentos{"./documentos"};
+    ifstream {documentos};
+    string line;
+
+    for (auto const& dir_entry : filesystem::directory_iterator{documentos}) {
+        while(getline(dir_entry, line))
+            for(auto palavra : line)
+                inserir(palavra, dir_entry);
+    }
 }
 
 string normalizar(string palavra) {
