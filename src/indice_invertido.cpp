aebb06e5f148c188971d44ef74d8ad32030d76e6
Luiz Fernando <luizfgrocha04@gmail.com>
2022-11-10 15:52:41 -0300

    Mais algumas melhoras em normalizar()

--- src/indice_invertido.cpp
+++ src/indice_invertido.cpp
@@ -14,8 +14,8 @@ void Indice_Invertido::inserir(string palavra, string documento) {
     indice_[palavra].insert(documento);
 }
 
-void Indice_Inverso::constroiIndice(){
-    const filesystem::path documentos{"./documentos"};
+void Indice_Invertido::constroiIndice() {
+  const filesystem::path documentos{"./documentos"};
     ifstream {documentos};
     string line;
 
@@ -27,11 +27,19 @@ void Indice_Inverso::constroiIndice(){
 }
 
 string normalizar(string palavra) {
+    string comAcentoOuCedilha = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+    string semAcentoOuCedilha = "aaaaaaaaaaaeeeeeeeeiiiiiiiioooooooooouuuuuuucc";
+
     string normalizada;
-    for (char c : palavra) {
-        if (tolower(c) >= 97 && tolower(c) <= 122) {
-            normalizada.push_back(tolower(c));
+    int index;
+    for (auto itr = palavra.begin(); itr != palavra.end(); itr++) {
+        if (index = comAcentoOuCedilha.find(*itr) != string::npos) {
+            normalizada.push_back(semAcentoOuCedilha[index]);
+            itr++;
+        } else if (tolower(*itr) >= 97 && tolower(*itr) <= 122){
+            normalizada.push_back(tolower(*itr));
         }
     }
+
     return normalizada;
 }
