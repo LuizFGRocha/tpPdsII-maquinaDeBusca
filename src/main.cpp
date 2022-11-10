1fde5a49429d255d53e817716861506e9e8f49e9
Luiz Fernando <luizfgrocha04@gmail.com>
2022-11-10 13:14:10 -0300

    Preparada para testar com arquivos dados.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
+#include "indice_invertido.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    Indice_Invertido google;
+    google.constroiIndice();
+    auto resultados = google.busca("apartamento");
+    for (string documento : resultados) {
+        cout << documento << endl;
+    }
+    return 0;
+}
