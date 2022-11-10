3d8c159c64d66017d4a142d57385cb1f6db98483
Luiz Fernando Gonçalves Rocha <108113333+LuizFGRocha@users.noreply.github.com>
2022-11-10 11:59:45 -0300

    Limpeza da main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-#include "indice_invertido.h"
 
-using namespace std;
-
-int main() {
-    if (normalizar("AaEe---!!!???12354") == "aaee") {
-        cout << "Correto" << endl;
-    }
-    return 0;
-}
